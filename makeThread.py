ca05d6c3b901b55d94286c45578eaa44dbe3b8d6
Andrew <andrew@reifman.net>
2018-02-23 18:05:40 +0000

    Updated thread to work when Guest doesn't exist on wikipedia

--- makeThread.py
+++ makeThread.py
@@ -45,69 +45,72 @@ def loginBot():
         sleep(10)
 
 
-def parseGuest(guest):
-    website = "https://en.wikipedia.org"+guest
+def parseGuest(guest,guestHtml):
+    website = "https://en.wikipedia.org"+guestHtml
     guestWebsite = requests.get(website, timeout=15)
     guest_html = guestWebsite.text
-    #guestInfo = guest_html.split('</table>')[1]
-    guestInfo = guest_html.split('<div id=\"mw-content-text\"')[1]
-    #paragraph = re.findall('<p>(.*?)</p>',guestInfo)[0]
-    paragraph = re.findall('<p>(.*?)</p>',guestInfo)
+    soup = BeautifulSoup(guest_html, 'lxml')
+    print soup.prettify()
+    content = soup.find("div",{"id":"mw-content-text"})
+    summary = content.findAll("p")
     text = ""
-    for p in paragraph[0:2]:
-        soup = BeautifulSoup(p, "lxml")
-        html = soup.get_text()
-        text += re.sub(r'(\[[0-9]?[0-9]?\])','',html)
-        text += '\n\n>'
-    #soup = BeautifulSoup(paragraph, "lxml")
-    #soup = soup.replace("(\[[0-9]?[0-9]?\])","")
+    for x in range(0,2):
+        text += ">"+summary[x].getText()
+        text += "\n\n"
+    text = re.sub(r'(\[[0-9]?[0-9]?\])','',text)
+    header = "["+guest.getText()+"]("+website+")\n\n"
+    header += text
+    print header
     return text
 
 def parseDescription(description):
     soup = BeautifulSoup(description, 'lxml')
     return soup.get_text()
     
-def parseEpisode(episode):
+def parseEpisode(index,episode):
     body = ""
     description = "#Description\n\n"
-    try:
-        desc = ">"+re.findall('<td class=\"description\".*?>(.*)<\/td>',episode)[0]
-        description += parseDescription(desc)
-        
-    except:
-        print getTimestamp() + "No description yet."
-        description += "No description available yet."
-    body += "\n\n"+description
-    guest = "#Tonight's Guest\n\n"
-    guestName = re.findall('<td><a href=.*?>(.*?)<\/a>',episode)[0]
-    tempGuest = re.findall('<td><a href=\"(.*)\" title',episode)[0]
-    guestInfo = parseGuest(tempGuest)
-    guestLink = "https://en.wikipedia.org"+tempGuest
-    guest += "["+guestName+"]("+guestLink+")"
-    body += "\n\n"+guest
-    body += "\n\n>"+guestInfo
-    return guestName,body
+    body += "\n\n"+description+"\n\n"
+    body += "#Tonight's Guest\n\n"
+    return body
     
 
 def getEpisode():
-    #today = time.strftime("%Y-%m-%d")
     today = str(datetime.datetime.now().year)+'-'
     today += str(datetime.datetime.now().month)+'-' if len(str(datetime.datetime.now().month)) > 1 else '0' +  str(datetime.datetime.now().month)+'-'
-    today += str(datetime.datetime.now().day - 1) if len(str(datetime.datetime.now().day)) > 1 else '0' +  str(datetime.datetime.now().day - 1)
-    website = "https://en.wikipedia.org/wiki/List_of_The_Opposition_with_Jordan_Klepper_episodes"
+    today += str(datetime.datetime.now().day - 1) if len(str(datetime.datetime.now().day - 1)) > 1 else '0' +  str(datetime.datetime.now().day - 1)
+    month = calendar.month_name[datetime.datetime.now().month]
+    website = "https://en.wikipedia.org/wiki/List_of_The_Opposition_with_Jordan_Klepper_episodes#"+month
     tableWebsite = requests.get(website, timeout=15)
     table_html = tableWebsite.text
-    table = table_html.split('<table ')[1]
-    episodes = table.split('<tr class')[1:]
-    for episode in episodes:
-        episodeDate = re.findall('\(<span.*?>(.*)<\/span>\)',episode)[0]
-        if today == episodeDate:
-            guest, body = parseEpisode(episode)
+    soup = BeautifulSoup(table_html, 'lxml')
+    month+= ' '+str(datetime.datetime.now().day)+','
+    table = soup.findAll("table",{"class": "wikitable plainrowheaders wikiepisodetable"})[-1]
+    episodes = table.findAll("tr",{"class":"vevent"})
+    for index,episode in enumerate(episodes):
+        episodeDate = episode.find("td", {"class":"summary"}).getText()
+        episodeDate = re.findall('\((.*)\)',episodeDate)[0]
+        if today in episodeDate:
+            body = parseEpisode(index,episode)
+            guest = episode.findAll("td")[1]
+            try:
+                guestLink = guest.find('a')
+                guestLink = guestLink['href']
+                print guestLink
+            except:
+                pass
+            if guestLink:
+                guestText = parseGuest(guest,guestLink )
+                body += guestText
+            else:
+                body += guest.getText()+"\n\n"
+        
+
     titleDate = datetime.datetime.now().month 
     titleDate = calendar.month_name[titleDate]+ " "
     titleDate += str(datetime.datetime.now().day - 1)+", " if len(str(datetime.datetime.now().day)) > 1 else '0' +  str(datetime.datetime.now().day - 1)+", "
     titleDate += str(datetime.datetime.now().year)
-    title = titleDate + " | The Opposition with Jordan Klepper | "+guest 
+    title = titleDate + " | The Opposition with Jordan Klepper | "+guest.getText()
     return title,body
 
 def postThread():
@@ -117,7 +120,6 @@ def postThread():
     submission = r.subreddit(subreddit).submit(title,selftext=body,send_replies=False)
     submission.mod.sticky(state=True)
     shortlink = submission.shortlink
-    print shortlink
     print getTimestamp() + "Thread Submitted"
     return submission
 
@@ -125,7 +127,6 @@ def updateThread(link):
     title,body = getEpisode()
     r,admin,username,password,subreddit,user_agent,id,secret,redirect = loginBot()
     print getTimestamp() + "Updating thread"
-    #thread = r.submission(link)
     link.edit(body)
 
 shortlink = postThread()
